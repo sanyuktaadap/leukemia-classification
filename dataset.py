a160c60717e71f9298114b7e59df5b41201f2a2e
sanyuktaadap <sanyuadap@gmail.com>
2023-08-29 14:49:08 -0700

    Updating fiilename

---
+++ dataset.py
@@ -0,0 +1,78 @@
+# Retrieve paths of data inside sub folders
+import glob
+import os
+# Image processing library
+import cv2
+import numpy as np
+# To create the Dataset class
+import torch
+from torch.utils.data import Dataset
+
+class LeukemiaDataset(Dataset):
+    """_summary_
+
+    Args:
+        Dataset : Parent torch dataset class
+    """
+    
+    def __init__(
+        self, 
+        imgs_path, 
+        img_dim,
+        transforms=None,
+        class_map={"benign" : 0, "early": 1, "pre": 2, "pro": 3},
+    ):
+        """Initializes a leukemia dataset instance
+
+        Args:
+            imgs_path (str): Path to the folder containing classwise images
+            img_dim (tuple): Tuple containing final image height and width
+            transforms (torchvision.transforms): Transforms to be run on an image
+            class_map (dict, optional): # Provides the mapping from the name to the number 
+                Defaults to {"Benign" : 0, "Early": 1, "Pre": 2, "Pro": 3}.
+        """
+    
+        # Path to training data
+        self.imgs_path = imgs_path
+        self.class_map = class_map
+        self.img_dim = img_dim
+        self.transforms = transforms
+        # List containing paths to all the images
+        self.data = []
+        
+        # List of all folders inside imgs_path
+        file_list = glob.glob(os.path.join(self.imgs_path, "*"))
+        # Iterate over all the classes in file list
+        for class_path in file_list:
+            # For each class, extract the actual class name
+            class_path = class_path.rstrip('/\\')
+            class_name = class_path.split('\\')[-1].lower()
+            # Retrieve each image (.jpg) in class folders
+            for img_path in glob.glob(os.path.join(class_path, "*")):
+                # self.data will contain path to each image, the respective class
+                self.data.append((img_path, class_name))
+        
+    def __len__(self):
+        """Gets the lengtht of the dataset
+
+        Returns:
+            int: length of the dataset
+        """
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        # idx - indexing data for accessibility 
+        img_path, class_name = self.data[idx]
+        # Loads an image from the given image_path
+        # OpenCV reads image by default as BGR
+        # Torch wants it to be RGB
+        img_tensor = cv2.cvtColor(cv2.imread(img_path), cv2.BGR2RGB)
+        # Assigning ids to each class (number, not name of the class)
+        class_id = self.class_map[class_name]
+        # Convert numpy array to a torch tensor
+        # img_tensor = torch.from_numpy(img)
+        # NumPy: BGR (Width, Height, Channels)
+        # Permute function changes the ordering of the dimensions (012 to 201)
+        # img_tensor = img_tensor.permute(2, 0, 1)
+        class_id = torch.tensor([class_id])
+        return img_tensor, class_id
