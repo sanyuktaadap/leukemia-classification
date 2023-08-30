24dffa22ee02a58d1793e2ff7ed07cf45bc2d00d
sanyuktaadap <sanyuadap@gmail.com>
2023-08-29 21:44:48 -0700

    Minor bug fixes

--- dataset.py
+++ dataset.py
@@ -11,23 +11,19 @@ class LeukemiaDataset(Dataset):
     def __init__(
         self, 
         imgs_path, 
-        img_dim,
         transforms=None,
         class_map={"benign" : 0, "early": 1, "pre": 2, "pro": 3},
     ):
         """Initializes a leukemia dataset instance
         Args:
             imgs_path (str): Path to the folder containing classwise images
-            img_dim (tuple): Tuple containing final image height and width
             transforms (torchvision.transforms): Transforms to be run on an image
             class_map (dict, optional): # Provides the mapping from the name to the number 
                 Defaults to {"Benign" : 0, "Early": 1, "Pre": 2, "Pro": 3}.
         """
-        
         # Path to training data
         self.imgs_path = imgs_path
         self.class_map = class_map
-        self.img_dim = img_dim
         self.transforms = transforms
         # List containing paths to all the images
         self.data = []
@@ -37,7 +33,7 @@ class LeukemiaDataset(Dataset):
         for class_path in file_list:
             # For each class, extract the actual class name
             class_path = class_path.rstrip('/\\')
-            class_name = class_path.split('\\')[-1].lower()
+            class_name = class_path.split('\\')[-1].split('/')[-1].lower()
             # Retrieve each image (.jpg) in class folders
             for img_path in glob.glob(os.path.join(class_path, "*")):
                 # self.data will contain path to each image, the respective class
@@ -45,7 +41,6 @@ class LeukemiaDataset(Dataset):
         
     def __len__(self):
         """Gets the length of the dataset
-
         Returns:
             int: total number of data points
         """
